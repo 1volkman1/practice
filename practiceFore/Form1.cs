36ef5466e867098eaa1f74cfaff5fba22d33ade5
1volkman1 <igarodin@gmail.com>
2014-07-16 17:09:02 +0300

    practiceFirst.sln - add new class and call function blablabla() in button_click1

---
+++ practiceFore/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practiceFore
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int len = Talker.BlahBlahBlah(textBox1.Text, (int)numericUpDown1.Value);
+            MessageBox.Show("The message lenght is " + len);
+        }
+
+
+    }
+}
