d4abcc17e3091a942decd0b7493777521c9ac8eb
1volkman1 <igarodin@gmail.com>
2014-07-29 00:37:54 +0300

    Swap.csproj

---
+++ Swap/Elephant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Swap
+{
+    class Elephant
+    {
+        public string Name;
+        public int EarSize;
+
+        public void WhoIAm()
+        {
+            MessageBox.Show("My ears are " + EarSize + " inches tail.", Name + " says ...");
+        }
+
+        public void TellMe(string message, Elephant WhoSaidIt)
+        {
+            MessageBox.Show(WhoSaidIt.Name + " says: " + message);
+        }
+        public void SpeakTo(Elephant WhoTolkMe,string message)
+        {
+            WhoTolkMe.TellMe(message, this);
+        }
+
+    }
+}
---
+++ Swap/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Swap
+{
+    public partial class Form1 : Form
+    {
+        Elephant lucinda;
+        Elephant Lloyd;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            lucinda = new Elephant() { Name = "Lucinda", EarSize = 33 };
+            Lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Lloyd.WhoIAm();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lucinda.WhoIAm();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Elephant holder;
+            holder = Lloyd;
+            Lloyd = lucinda;
+            lucinda = holder;
+            MessageBox.Show("Animals was swap!");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Lloyd.SpeakTo(lucinda, "Hello");
+        }
+    }
+}
