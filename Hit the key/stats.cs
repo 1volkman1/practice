4ba666a2d750663546513c273e019e771fb1844a
1volkman1 <igarodin@gmail.com>
2014-07-30 00:11:49 +0300

    игра печатающая машинка.Hit the key.csproj

---
+++ Hit the key/stats.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hit_the_key
+{
+    class stats
+    {
+        public int Total = 0;
+        public int MIssed = 0;
+        public int Correct = 0;
+        public int Accuracy = 0;
+
+        public void Update(bool correctkey)
+        {
+            Total++;
+
+            if (!correctkey)
+            {
+                MIssed++;
+            }
+            else
+            {
+                Correct++;
+            }
+
+            Accuracy = 100 * Correct / (MIssed + Correct);
+        }
+    }
+}
