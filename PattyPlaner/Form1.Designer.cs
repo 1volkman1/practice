ee6c7cfe2e7a5d5c17cdfd432134efdaa8ffc23a
1volkman1 <igarodin@gmail.com>
2014-08-01 00:13:45 +0300

    partyplanner

---
+++ PattyPlaner/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace PattyPlaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.fancyBox = new System.Windows.Forms.CheckBox();
+            this.healthBox = new System.Windows.Forms.CheckBox();
+            this.labelCost = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of People";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(25, 50);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // fancyBox
+            // 
+            this.fancyBox.AutoSize = true;
+            this.fancyBox.Checked = true;
+            this.fancyBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.fancyBox.Location = new System.Drawing.Point(25, 94);
+            this.fancyBox.Name = "fancyBox";
+            this.fancyBox.Size = new System.Drawing.Size(115, 17);
+            this.fancyBox.TabIndex = 2;
+            this.fancyBox.Text = "Fancy Decorations";
+            this.fancyBox.UseVisualStyleBackColor = true;
+            this.fancyBox.CheckedChanged += new System.EventHandler(this.fancyBox_CheckedChanged);
+            // 
+            // healthBox
+            // 
+            this.healthBox.AutoSize = true;
+            this.healthBox.Location = new System.Drawing.Point(25, 118);
+            this.healthBox.Name = "healthBox";
+            this.healthBox.Size = new System.Drawing.Size(101, 17);
+            this.healthBox.TabIndex = 3;
+            this.healthBox.Text = "Healthy Options";
+            this.healthBox.UseVisualStyleBackColor = true;
+            this.healthBox.CheckedChanged += new System.EventHandler(this.healthBox_CheckedChanged);
+            // 
+            // labelCost
+            // 
+            this.labelCost.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelCost.Location = new System.Drawing.Point(104, 167);
+            this.labelCost.Name = "labelCost";
+            this.labelCost.Size = new System.Drawing.Size(100, 23);
+            this.labelCost.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(25, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Cost";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelCost);
+            this.Controls.Add(this.healthBox);
+            this.Controls.Add(this.fancyBox);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.CheckBox fancyBox;
+        private System.Windows.Forms.CheckBox healthBox;
+        private System.Windows.Forms.Label labelCost;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PattyPlaner/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PattyPlaner
+{
+    public partial class Form1 : Form
+    {
+        DinnerParty dinnerParty;
+
+        public Form1()
+        {
+            InitializeComponent();
+            dinnerParty = new DinnerParty() ;
+            dinnerParty.CalculateCostOfDecorations(fancyBox.Checked);
+            dinnerParty.SetHealthyOption(healthBox.Checked);
+            DisplayDinnerPartyCost();
+           
+        }
+
+        private void fancyBox_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerParty.CalculateCostOfDecorations(fancyBox.Checked);
+            DisplayDinnerPartyCost();
+        }
+
+        private void healthBox_CheckedChanged(object sender, EventArgs e)
+        {
+            dinnerParty.SetHealthyOption(healthBox.Checked);
+            DisplayDinnerPartyCost();
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            dinnerParty.SetPartyOptions((int)numericUpDown1.Value, fancyBox.Checked);
+            DisplayDinnerPartyCost();
+        }
+
+        private void DisplayDinnerPartyCost()
+        {
+            decimal Cost = dinnerParty.CalculatorCost(healthBox.Checked);
+            labelCost.Text = Cost.ToString("c");
+        }
+        
+    }
+}
